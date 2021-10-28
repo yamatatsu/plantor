466d20767e254e0ab470ece2c5aa869743091f10
yamatatsu <the.kinnikuman@gmail.com>
2021-10-28 10:28:57 +0900

    test

---
+++ packages/cdk/lib/NatureRemo.test.ts
@@ -0,0 +1,11 @@
+import { SynthUtils } from "@aws-cdk/assert";
+import { App } from "aws-cdk-lib";
+import { NatureRemo } from "./NatureRemo";
+
+test("snapshot test", () => {
+  const app = new App();
+
+  const target = new NatureRemo(app, "Target", {});
+
+  expect(SynthUtils.toCloudFormation(target)).toMatchSnapshot();
+});
