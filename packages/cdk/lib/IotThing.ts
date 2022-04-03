6df6e99677a08c1284de6d917277faf5abd49710
yamatatsu <the.kinnikuman@gmail.com>
2022-04-03 16:52:59 +0900

    refactor

--- packages/cdk/lib/IotThing.ts
+++ packages/cdk/lib/IotThing.ts
@@ -1,21 +1,22 @@
-import { App, Stack, StackProps, aws_iot } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import * as iot from "aws-cdk-lib/aws-iot";
 
-type Props = StackProps & {
+type Props = cdk.StackProps & {
   clientCertArn: string;
 };
 
-export class IotThing extends Stack {
-  constructor(parent: App, id: string, props: Props) {
+export class IotThing extends cdk.Stack {
+  constructor(parent: cdk.App, id: string, props: Props) {
     super(parent, id, props);
 
     const { clientCertArn } = props;
 
-    const thing = new aws_iot.CfnThing(this, "Thing", {
+    const thing = new iot.CfnThing(this, "Thing", {
       thingName: "wio_terminal_01",
       attributePayload: {},
     });
 
-    const policy = new aws_iot.CfnPolicy(this, "Policy", {
+    const policy = new iot.CfnPolicy(this, "Policy", {
       policyName: "wio_terminal_policy",
       policyDocument: {
         Version: "2012-10-17",
@@ -42,7 +43,7 @@ export class IotThing extends Stack {
      * これにより、証明書が権限を持つ。（認可）
      * この設定がないと、証明書があってもmqttとかは認証エラーとなる。
      */
-    new aws_iot.CfnPolicyPrincipalAttachment(this, "PPA", {
+    new iot.CfnPolicyPrincipalAttachment(this, "PPA", {
       policyName: policy.policyName!,
       principal: clientCertArn,
     });
@@ -51,7 +52,7 @@ export class IotThing extends Stack {
      * これにより、証明書が thing とマッピングする。
      * この設定がないと AWS IoT は mqtt を受け取っても各機能に紐付けられない。
      */
-    new aws_iot.CfnThingPrincipalAttachment(this, "TPA", {
+    new iot.CfnThingPrincipalAttachment(this, "TPA", {
       thingName: thing.thingName!,
       principal: clientCertArn,
     });
